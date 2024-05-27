08e2f333235fc1de67fb80291e48d2d3a928988b
nikhilesh2010 <nikhilesh.cc@gmail.com>
2024-05-28 00:27:47 +0530

    Changes made using Sanity for Auth and configured

--- src/app/(web)/auth/page.tsx
+++ src/app/(web)/auth/page.tsx
@@ -31,7 +31,7 @@ const Auth = () => {
 
   return (
     <section className="container mx-auto">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-50 md:w-[40%] mx-auto">
         <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
           <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">Create an account</h1>
           <p>OR</p>
